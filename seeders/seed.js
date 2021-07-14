6b8edb682e63cb91f62524ea96c61308f8acca76
jugrajn <jugraj.nijjar@gmail.com>
2021-07-14 15:22:53 -0400

    fixed db seed

--- seeders/seed.js
+++ seeders/seed.js
@@ -125,8 +125,8 @@ const workoutSeed = [
   },
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then((data) => {
     console.log(data.result.n + ' records inserted!');
     process.exit(0);
